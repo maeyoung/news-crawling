b2a178694d2741dbd5153433d2dc1617e204d112
boramaey <boramaey@gmail.com>
2021-07-20 00:08:10 +0900

    edit date part in google_bild.py

--- google_bild.py
+++ google_bild.py
@@ -10,24 +10,31 @@ column_list = ["country", "media", "date", "headline", "article", "url"]
 df = pd.DataFrame(columns=column_list)
 
 # site:https://www.bild.de/ 2010..2020 korea
-url = 'https://www.google.com/search?q=site%3Ahttps%3A%2F%2Fwww.bild.de%2F+2010..2020+korea&ei=KyvtYNHIFYyC-AbxqYDYAw&oq=site%3Ahttps%3A%2F%2Fwww.bild.de%2F+2010..2020+korea&gs_lcp=Cgdnd3Mtd2l6EANKBAhBGAFQqgpY3CFg3yJoAXAAeACAAdMBiAGSE5IBBjAuMjAuMZgBAKABAaoBB2d3cy13aXrAAQE&sclient=gws-wiz&ved=0ahUKEwiRitm_rt_xAhUMAd4KHfEUADsQ4dUDCA4&uact=5'
+url = 'https://www.google.com/search?q=site%3Ahttps%3A%2F%2Fwww.bild.de%2F+korea&biw=1792&bih=1008&source=lnt&tbs=cdr%3A1%2Ccd_min%3A1%2F1%2F2010%2Ccd_max%3A12%2F31%2F2020&tbm='
 driver = webdriver.Chrome(executable_path='./chromedriver')
 
 # remove cookie banner
 # driver.get('https://www.bild.de/suche.bild.html?query=korea')
 driver.get(url)
 
-def get_href(hrefs):
+def get_href():
+    hrefs = []
     try:
         html = driver.find_elements_by_class_name('yuRUbf')
         for elem in html:
             href = elem.find_element_by_tag_name('a').get_attribute('href')
             hrefs.append(href)
+        # print(hrefs)
         return (hrefs)
 
     except KeyboardInterrupt or NoSuchElementException:
         print('Error')
         driver.close()
+
+def process_datetime(info):
+    date = info[:10]
+    time = info[11:19]
+    return date + " " + time
 
 def get_data(hrefs):
     try:
@@ -43,12 +50,9 @@ def get_data(hrefs):
                 driver.close()
                 driver.switch_to.window(driver.window_handles[0])
                 continue
-            n_date = driver.find_element_by_class_name("authors__pubdate").get_attribute("datetime")
-            # n_date[0:10]
-            
-            # 날짜 양식 변경하기~!~!~!~!~!~!~!~!~!~!
-            print(n_date)
-            # datetime.strptime(n_date, "%Y-%m-%d %H:%M:%S")
+            n_date = driver.find_element_by_class_name("authors__pubdate").get_attribute("datetime")            
+            n_date = process_datetime(n_date)
+            # print(n_date)
             data.append(n_date)
 
             # 중간에 <br>있는 경우 '\n'로 들어가는거 처리하기~!~!~!~!~!~!
@@ -107,12 +111,10 @@ def check_exist_button(b_name):
 
 
 if __name__ == '__main__':
-    hrefs = []
-    hrefs = get_href(hrefs)
+    hrefs = get_href()
     get_data(hrefs)
     while check_exist_button('pnnext'):
-        hrefs = []
-        hrefs = get_href(hrefs)
+        hrefs = get_href()
         get_data(hrefs)
     xlxs_dir = "./bild.xlsx"
     writer = pd.ExcelWriter(xlxs_dir, engine='xlsxwriter')
