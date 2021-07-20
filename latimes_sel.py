51ea069a82688ea26b6aabc7870be8e5b5bb86c8
boramaey <boramaey@gmail.com>
2021-07-20 17:45:36 +0900

    fix check_is_exist function, get data information part, ignore no headline news part

--- latimes_sel.py
+++ latimes_sel.py
@@ -25,6 +25,7 @@ results['article'] = list()
 results['url'] = list()
 
 url = "https://www.google.com/search?q=site%3Awww.latimes.com+korea&sxsrf=ALeKk00edGqjzjJ3fIEzZacIoUF9oPl3ag%3A1626706287442&source=lnt&tbs=cdr%3A1%2Ccd_min%3A1%2F1%2F2010%2Ccd_max%3A12%2F31%2F2020&tbm="
+# test = 'https://www.google.com/search?q=site%3Awww.latimes.com+korea+photo&ei=RXT2YOeFJdSSoATFoaLwCg&oq=site%3Awww.latimes.com+korea+photo&gs_lcp=Cgdnd3Mtd2l6EANKBAhBGAFQ8QpYyxVgxhZoAXAAeACAAegBiAGjB5IBBTEuNS4xmAEAoAEBqgEHZ3dzLXdpesABAQ&sclient=gws-wiz&ved=0ahUKEwinvKy9ifHxAhVUCYgKHcWQCK4Q4dUDCA4&uact=5'
 driver = webdriver.Chrome(executable_path='./chromedriver')
 driver.implicitly_wait(time_to_wait=5)
 driver.get(url=url)
@@ -43,14 +44,12 @@ def get_href_date():
             body = elem.find_element_by_class_name('IsZvec')
             href = head.find_element_by_tag_name('a').get_attribute('href')
             date = ""
-            if check_is_exist("class", 'MUxGbd.wuQ4Ob.WZ8Tjf'):
+            if check_is_exist(body, "class", 'MUxGbd.wuQ4Ob.WZ8Tjf'):
                 date = body.find_element_by_class_name('MUxGbd.wuQ4Ob.WZ8Tjf').get_attribute("textContent")
             hrefs.append(href)
+            if "전" in date:
+                date = ""
             dates.append(process_datetime(2, date))
-        # html = driver.find_elements_by_class_name('yuRUbf')
-        # for elem in html:
-        #     href = elem.find_element_by_tag_name('a').get_attribute('href')
-        #     hrefs.append(href)
         return hrefs, dates
 
     except KeyboardInterrupt or NoSuchElementException:
@@ -72,11 +71,14 @@ def process_datetime(type, info):
         if info != "":
             date_obj = datetime.datetime.strptime(info, "%Y. %m. %d. — ")
             date = date_obj.strftime("%Y-%m-%d")
-        return date + " " + "00:00:00"
+            return date + " " + "00:00:00"
+        else:
+            return "no date information"
 
 
 def get_content():
     date = 0
+    headline = 0
     content = ""
     try:
         body = driver.find_element_by_class_name("rich-text-article-body")
@@ -85,7 +87,7 @@ def get_content():
         headline = title.find_element_by_class_name("headline").get_attribute("textContent").strip()
         for b in article:
             content += b.get_attribute("textContent").strip()
-        if check_is_exist("class", "byline"):
+        if check_is_exist(driver, "class", "byline"):
             date = driver.find_element_by_class_name("byline").find_element_by_tag_name("time").get_attribute("datetime")
             date = process_datetime(0, date)
         return [headline, date, content]
@@ -96,13 +98,16 @@ def get_content():
 def get_html(hrefs, dates):
     try:
         for link, auth_date in zip(hrefs, dates):
-            # continue
             driver.execute_script("window.open();")
             driver.switch_to.window(driver.window_handles[-1])
             driver.get(url=link)
             time.sleep(3)
 
             [title, date, content] = get_content()
+            if title == 0:
+                driver.close()
+                driver.switch_to.window(driver.window_handles[0])
+                continue
             if date == 0:
                 # to-do: get date info from google lists
                 date = auth_date
@@ -125,12 +130,12 @@ def get_html(hrefs, dates):
         driver.close()
     return results
 
-def check_is_exist(type, name):
+def check_is_exist(window, type, name):
     try:
         if (type == "class"):
-            driver.find_element_by_class_name(name)
+            window.find_element_by_class_name(name)
         elif (type == "id"):
-            driver.find_element_by_id(name)
+            window.find_element_by_id(name)
     except NoSuchElementException:
         return False
     return True
@@ -147,13 +152,9 @@ def check_exist_button(b_name):
 
 
 if __name__ == '__main__':
-    # hrefs = []
-    # dates = []
     hrefs, dates = get_href_date()
     csv = get_html(hrefs, dates)
     while check_exist_button('pnnext'):
-        # hrefs = []
-        # dates = []
         hrefs, dates = get_href_date()
         csv = get_html(hrefs, dates)
     dict_to_df = pd.DataFrame.from_dict(csv)
