1b787043a278f1ddfc2a1c2e38a7ef8a05c24f50
boramaey <boramaey@gmail.com>
2021-09-05 21:56:44 +0900

    modify baidu next button error

--- baidu_huanqiu.py
+++ baidu_huanqiu.py
@@ -1,6 +1,5 @@
-import sys
-
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 import time
@@ -10,10 +9,11 @@ import sys, traceback
 import logging
 logging.basicConfig(level=logging.ERROR)
 
-driver = webdriver.Chrome(executable_path='./chromedriver')
-# driver.get(url=url)
+caps = DesiredCapabilities().CHROME
+caps["pageLoadStrategy"] = "eager" # interactive
+driver = webdriver.Chrome(desired_capabilities=caps, executable_path='./chromedriver')
 
-init_results = {'country': list(), 'media': list(), 'date': list(), 'headline': list(), 'article': list(), 'url': list()}
+# init_results = {'country': list(), 'media': list(), 'date': list(), 'headline': list(), 'article': list(), 'url': list()}
 
 date_word = ["年", "月", "日", "時", "分"]
 
@@ -50,9 +50,6 @@ def get_href_date():
 
 def process_datetime(type, info):
     if type == 0:
-        # date = info[:10]
-        # time = info[11:19]
-        # return date + " " + time
         return info + ":00"
     elif type == 1:
         if info != "":
@@ -61,16 +58,6 @@ def process_datetime(type, info):
             return date + " " + "00:00:00"
         else:
             return "no date information"
-    # elif type == 2:
-    #     for word in date_word:
-    #         info = info.replace(word, ' ')
-    #     info = info.split()
-    #     print(info)
-    #     date = info[0]+'-'+info[1]+'-'+info[2]
-    #     # time = info[3]+':'+info[4]+':00'
-    #     time = "00:00:00"
-    #     dt = datetime.strptime(date+' '+time, '%Y-%m-%d %H:%M:%S')
-    #     return dt
 
 
 def check_is_exist(window, type, name):
@@ -89,15 +76,24 @@ def check_exist_button(b_name):
     try:
         btns = driver.find_elements_by_class_name(b_name)
         next = btns[-1]
-        next.send_keys(Keys.ENTER)
-        time.sleep(3)
-    except NoSuchElementException:
+        btn_text = next.get_attribute("textContent")
+        print(btn_text)
+        # 下一页 >
+        if btn_text == "下一页 >":
+            next.send_keys(Keys.ENTER)
+            time.sleep(3)
+            return True
+        else:
+            return False
+    except NoSuchElementException as nse:
+        print(nse)
         return False
-    except IndexError:
+    except IndexError as i:
+        print(i)
         return False
-    return True
+    # return True
 
-def get_data(hrefs, dates, results):
+def get_data(year, hrefs, dates, results):
     try:
         for link, auth_date in zip(hrefs, dates):
             driver.execute_script("window.open();")
@@ -116,15 +112,13 @@ def get_data(hrefs, dates, results):
                 driver.switch_to.window(driver.window_handles[0])
                 continue
 
-            # print(results)
-
             # date
             if check_is_exist(driver, "class", "time"):
                 date = driver.find_element_by_class_name("time").get_attribute("textContent")
                 date = process_datetime(0, date)
             else:
                 date = auth_date
-            # print(results)
+
             # headline
             title = driver.find_element_by_class_name("t-container-title")
             if check_is_exist(title, "tag", "h3") == False:
@@ -132,7 +126,7 @@ def get_data(hrefs, dates, results):
                 driver.switch_to.window(driver.window_handles[0])
                 continue
             title = title.find_element_by_tag_name("h3").text
-            # print(results)
+
             # article 
             body = driver.find_element_by_class_name("l-con.clear")
             article = body.find_elements_by_tag_name("p")
@@ -145,40 +139,38 @@ def get_data(hrefs, dates, results):
                 driver.switch_to.window(driver.window_handles[0])
                 continue
 
-            # print(results)
             results['country'].append('China')
             results['media'].append('Huanqiu')
             results['date'].append(date)
             results['headline'].append(title)
             results['article'].append(content)
             results['url'].append(cur_url)
-            # print(results)
-
 
             driver.close()
             driver.switch_to.window(driver.window_handles[0])
 
-            if len(hrefs) < 10:
-                time.sleep(10)
+        if len(hrefs) < 10:
+            time.sleep(20)
+            print("time wait.....")
 
     except:
-        data_save("error", results)
+        data_save(year, results)
         driver.close()
         driver.switch_to.window(driver.window_handles[0])
-        print("현재 데이터까지 저장완료")
-        print("에러 위치 : " + cur_url)
+        print("\n\n현재 데이터까지 저장완료")
+        print("에러 위치 : " + cur_url + "\n\n")
         logging.error(traceback.print_exc())
-        # return results
 
 
 if __name__ == '__main__':
     years = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
     cnt = 0
     start = time.time()
-    results = init_results
+    results = {'country':list(), 'media': list(), 'date': list(), 'headline': list(), 'article':list(), 'url': list()}
     try:
         for year in years:
-            results = init_results
+            results = {'country':list(), 'media': list(), 'date': list(), 'headline': list(), 'article':list(), 'url': list()}
+
             month = 1
             while month < 13:
                 s_min = "01/"+str(month)+"/"+str(year)+" 00:00:00"
@@ -192,30 +184,26 @@ if __name__ == '__main__':
                 d_max = datetime.strptime(s_max, "%d/%m/%Y %H:%M:%S")
                 r_max = time.mktime(d_max.timetuple())
                 maxdate = int(r_max)
-                # print('mindate : '+mindate+' maxdate : '+maxdate)
-                month += 1
+
                 search_url = "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=site%3Ahuanqiu.com%2F%20%2B%E9%9F%A9%E5%9B%BD&ct=2097152&si=huanqiu.com%2F&fenlei=256&oq=site%3Ahuanqiu.com%2F%20%2B%E9%9F%A9%E5%9B%BD&rsv_enter=1&rsv_dl=tb&gpc=stf%3D" + str(mindate) + "%2C" + str(maxdate) + "%7Cstftype%3D2&tfflag=1"
-                print(search_url)
+                print("year:" + str(year) + " month: " + str(month))
+                month += 1
                 driver.get(url=search_url)
                 time.sleep(3)
 
                 hrefs, dates = get_href_date()
-                get_data(hrefs, dates, results)
+                get_data(year, hrefs, dates, results)
                 while check_exist_button('n'):
                     hrefs, dates = get_href_date()
-                    get_data(hrefs, dates, results)
+                    get_data(year, hrefs, dates, results)
             data_save(year, results)
+            print(str(year)+"년 데이터 수집 완료")
 
     except:
-        print(sys.exc_info[0])
-        data_save("error", results)
-        # print("Error : " + e)
+        data_save(year, results)
+        print("\n\n현재 데이터까지 저장완료\n\n")
         logging.error(traceback.print_exc())
 
     finally:
-        print("최종소요시간: " + str(time.time() - start) + "초")
+        print("\n\n최종소요시간: " + str(time.time() - start) + "초\n\n")
         driver.close()
-
-
-
-# datetime(2010,1,2,0,0).strftime('%s')
